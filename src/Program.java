c7073eacc4422726761c0253ad0fe80a6a27750d
vargalb <lucasvargalvl@gmail.com>
2026-06-28 23:25:30 -0300

    iniciado banco de dados mysql na aplicação / start database mysql in application

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+import db.DB;
+
+import java.sql.Connection;
+
+public class Program {
+    static void main() {
+        Connection conn = DB.getConnection();
+
+        DB.closeConnection();
+    }
+}
